73870dbb972bdf9e0e2fbb5fa16afc9b12f2fffc
Subhash Soni <Subhash Soni>
2016-10-05 13:37:17 +0530

    Imade changes to server.js

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/sidebg.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sidebg.png'));
+});
+
+app.get('/ui/tcillogo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'tcillogo.png'));
 });
 
 
